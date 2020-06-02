ab51de53f1b9cd29a4780272f3d172a3d1dc22da
Pankaj 1 <pankajsur60@gmail.com>
2020-06-02 08:50:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+pictue = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+
+#1:Iterate over picture
+ #if 0 print empty-space 
+ #if 1 print *
+
+for row in pictue:
+  for pixel in row:
+    if pixel == 1:
+      print("*", end="")
+    else:
+      print("", end="")
+  print("")
